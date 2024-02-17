516c300d8010e50f44a86f4eced5b823090597a3
IanCist <144719417+IanCist@users.noreply.github.com>
2024-02-17 00:31:53 +0000

    fixed js code

--- Images/script.js
+++ Images/script.js
@@ -1,10 +1,12 @@
 function savePlayerName() {
     var playerName = document.getElementById("playerNameInput").value;
     localStorage.setItem("playerName", playerName);
+    document.getElementById("playerName").innerText = "Player Name: " + playerName;
     alert("Player name saved successfully!")
 }
 
 var savedPlayerName = localStorage.getItem("playerName")
 if(savedPlayerName) {
+    document.getElementById("playerName").innerText = "Player Name: " + savedPlayerName;
     alert ("Welcome back, " + savedPlayerName + "!");
 }
