f517e2788ec8896d916cf77fa2d99a41c290961a
IanCist <144719417+IanCist@users.noreply.github.com>
2024-02-29 01:10:30 +0000

    java script for player choice added

--- Images/script.js
+++ Images/script.js
@@ -10,18 +10,44 @@ function resetGame() {
     document.getElementById("gameResult").innerText = "";
 }
 var savedPlayerName = localStorage.getItem("playerName")
-if(savedPlayerName) {
+if (savedPlayerName) {
     document.getElementById("playerName").innerText = "Player Name: " + savedPlayerName;
-    alert ("Welcome back, " + savedPlayerName + "!");
+    alert("Welcome back, " + savedPlayerName + "!");
 }
 
-function computerChoice () {
+function computerChoice() {
     const choices = ["rock", "paper", "scissors"]
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
-function playGame(playerMove){
-const computerMove = computerChoice();
-console.log ("Computer's move:" + computerMove);
-console.log ("Player's move: " + playerMove)
+function playGame(playerMove) {
+    const computerMove = computerChoice();
+    console.log("Computer's move:" + computerMove);
+    console.log("Player's move: " + playerMove)
+}
+
+const rockButtons = document.querySelectorAll('.rock');
+const paperButtons = document.querySelectorAll('.paper');
+const scissorButtons = document.querySelectorAll('.scissor');
+
+rockButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        savePlayerChoice('rock');
+    });
+});
+
+paperButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        savePlayerChoice('paper');
+    });
+});
+
+scissorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        savePlayerChoice('scissors');
+    });
+});
+
+function savePlayerChoice(choice) {
+    document.getElementById("playerChoice").innerHTML = "Players choice: " + choice;
 }
